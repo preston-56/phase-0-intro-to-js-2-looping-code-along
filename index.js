647c058719dd72f168d03d2625a0691f650993e6
preston-56 <prestonosoro56@gmail.com>
2022-11-17 03:06:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+//writeCards() returns an array of thank you messages for each name provided to the function:
+function writeCards(names){
+    let messages=[];
+    for(let i=0;i< names.length;i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }   
+    return messages;
+}
+
+function countDown(n){
+    while(n>0){
+        console.log(n)
+        n--;
+    }
+    console.log(n)
+}
+    
